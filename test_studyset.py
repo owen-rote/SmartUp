c6ecead8e6161538a3ec61c766e8ecfaf06203fa
owen-rote <rotenbergowen@csu.fullerton.edu>
2024-10-22 15:03:49 -0700

    create StudySet class

---
+++ test_studyset.py
@@ -0,0 +1,10 @@
+# Test script to demonstrate StudySet class
+
+from StudySet import StudySet
+
+my_studyset = StudySet()
+
+my_studyset.createStudySet("new.json")
+
+print(my_studyset.studyset_dict)
+
