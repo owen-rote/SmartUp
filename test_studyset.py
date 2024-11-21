bb25066f8c062749fb9750d393776cb38674b50d
owen-rote <rotenbergowen@csu.fullerton.edu>
2024-11-21 13:15:06 -0800

    add original files
    accidentally removed

---
+++ test_studyset.py
@@ -0,0 +1,11 @@
+# Script to demonstrate StudySet class
+
+from StudySet import StudySet
+
+my_studyset = StudySet()
+
+my_studyset.createStudySet("new.json")
+
+print(my_studyset.studyset_dict)
+
+
